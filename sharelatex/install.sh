ce83faa2350fdcdd985c41e120e662f270473fd3
Gregor von Laszewski <laszewski@gmail.com>
2014-02-25 14:04:49 -0500

    updates

--- sharelatex/install.sh
+++ sharelatex/install.sh
@@ -30,10 +30,45 @@ echo "LATEX"
 echo "######################################################################"
 sudo apt-get install -y texlive
 
+sudo apt-get install -y latexmk
+
+
+mkdir ~/bin 
+curl http://mirror.physik-pool.tu-berlin.de/tex-archive/support/latexmk/latexmk.pl > ~/bin/latexmk
+chmod a+x ~/bin/latexmk
+export PATH=~/bin:$PATH
+
+#sudo apt-get -y install unzip
+#mkdir /tmp/a
+#cd /tmp/a
+#wget http://users.phys.psu.edu/~collins/software/latexmk-jcc/latexmk-440.zip
+#unzip latexmk-440.zip 
+
+
 #echo "######################################################################"
 #echo "Redis"
 #echo "######################################################################"
-# sudo apt-get install -y redis-server
+#sudo apt-get install -y redis-server
+
+mkdir /tmp/b
+cd /tmp/b
+wget http://redis.googlecode.com/files/redis-2.6.9.tar.gz
+tar -zxf redis-2.6.9.tar.gz
+cd redis-2.6.9
+make
+sudo make install
+wget https://github.com/ijonas/dotfiles/raw/master/etc/init.d/redis-server
+wget https://github.com/ijonas/dotfiles/raw/master/etc/redis.conf
+sudo mv redis-server /etc/init.d/redis-server
+sudo chmod +x /etc/init.d/redis-server
+sudo mv redis.conf /etc/redis.conf
+sudo mkdir -p /var/lib/redis
+sudo mkdir -p /var/log/redis
+sudo useradd --system --home-dir /var/lib/redis redis
+sudo chown redis.redis /var/lib/redis
+sudo chown redis.redis /var/log/redis
+sudo update-rc.d redis-server defaults
+sudo /etc/init.d/redis-server start
 
 echo "######################################################################"
 echo "Mongo"
@@ -41,7 +76,7 @@ echo "######################################################################"
 sudo apt-key -y  adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get -y  update
-sudo apt-get -y install mongodb-10gen
+sudo apt-get -y --force-yes install mongodb-10gen
 
 
 echo "######################################################################"
@@ -50,12 +85,25 @@ echo "######################################################################"
 sudo npm install -g grunt-cli
 
 echo "######################################################################"
+echo "emacs"
+echo "######################################################################"
+sudo apt-get -y install emacs23
+
+echo "######################################################################"
 echo "share latex"
 echo "######################################################################"
-
-
 
 git clone https://github.com/sharelatex/sharelatex.git
 cd sharelatex
 
 
+npm install
+grunt install
+
+grunt check --force
+
+
+
+#grunt run
+
+cp /vagrant_data/setttings.development.coffee sharelatex/config/settings.development.coffee
